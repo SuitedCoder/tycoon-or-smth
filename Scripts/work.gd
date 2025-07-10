cef7a5ac477a6f1401d13fd2c7364b7d14a8feb0
noob <107849135+SuitedCoder@users.noreply.github.com>
2025-07-11 06:52:19 +0700

    commit

--- Scripts/work.gd
+++
@@ -1,9 +0,0 @@
-extends Button
-
-@onready var label = get_node("../../projectsmenu/projects")
-@onready var clicksound = get_node("/root/Main/click")
-
-func _on_pressed() -> void:
-	clicksound.play()
-	if Details.projCurrent == true:
-			Details.projProgress += (5 / Details.projComplexity)
